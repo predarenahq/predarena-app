3f36f6a4fb86cfd767bfbc7f76ef2febd2299225
Oluwabukunmi <adeshipob@gmail.com>
2026-07-10 23:11:18 +0100

    chore: ignore scratch and backup files

--- derive_vault.mjs
+++
@@ -1,4 +0,0 @@
-import { PublicKey } from '@solana/web3.js'
-const PROGRAM_ID = new PublicKey('3mA18tJXtbTcp7eK3W7xENmqEjxReqCcBsBmUnHTg8RB')
-const [vault] = PublicKey.findProgramAddressSync([Buffer.from('platform_vault')], PROGRAM_ID)
-console.log('Vault PDA:', vault.toBase58())
