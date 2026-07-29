15ebfffcde47b6553bc41f8908416d28f09826e1
Oluwabukunmi <adeshipob@gmail.com>
2026-07-29 18:40:02 +0100

    arc: restore ERC20_ABI export dropped during abi.ts regen (broke the v3 ABI commit)

--- src/arc/abi.ts
+++ src/arc/abi.ts
@@ -1568,3 +1568,27 @@ export const PREDARENA_ABI = [
     "anonymous": false
   }
 ] as const;
+
+export const ERC20_ABI = [
+  {
+    type: "function",
+    name: "approve",
+    inputs: [{ name: "spender", type: "address" }, { name: "amount", type: "uint256" }],
+    outputs: [{ name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "allowance",
+    inputs: [{ name: "owner", type: "address" }, { name: "spender", type: "address" }],
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "balanceOf",
+    inputs: [{ name: "account", type: "address" }],
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+  },
+] as const
